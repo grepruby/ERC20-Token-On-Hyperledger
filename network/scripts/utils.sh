fcf296e92ac871daa11d468447529bad4cac06c9
Ayush Tiwari <ayush@techracers.com>
2018-09-06 18:00:16 +0530

    Added token network settings and setup helpers

---
+++ network/scripts/utils.sh
@@ -0,0 +1,194 @@
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/techracers.com/orderers/orderer.techracers.com/msp/tlscacerts/tlsca.techracers.com-cert.pem
+PEER0_ORG1_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.techracers.com/peers/peer0.org1.techracers.com/tls/ca.crt
+PEER0_ORG2_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.techracers.com/peers/peer0.org2.techracers.com/tls/ca.crt
+
+# verify the result of the end-to-end test
+verifyResult() {
+  if [ $1 -ne 0 ]; then
+    echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!"
+    echo "========= ERROR !!! FAILED to execute End-2-End Scenario ==========="
+    echo
+    exit 1
+  fi
+}
+
+# Set OrdererOrg.Admin globals
+setOrdererGlobals() {
+  CORE_PEER_LOCALMSPID="OrdererMSP"
+  CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/techracers.com/orderers/orderer.techracers.com/msp/tlscacerts/tlsca.techracers.com-cert.pem
+  CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/techracers.com/users/Admin@techracers.com/msp
+}
+
+setGlobals() {
+  PEER=$1
+  ORG=$2
+  if [ $ORG -eq 1 ]; then
+    CORE_PEER_LOCALMSPID="Org1MSP"
+    CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.techracers.com/users/Admin@org1.techracers.com/msp
+    if [ $PEER -eq 0 ]; then
+      CORE_PEER_ADDRESS=peer0.org1.techracers.com:7051
+    else
+      CORE_PEER_ADDRESS=peer1.org1.techracers.com:7051
+    fi
+  elif [ $ORG -eq 2 ]; then
+    CORE_PEER_LOCALMSPID="Org2MSP"
+    CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.techracers.com/users/Admin@org2.techracers.com/msp
+    if [ $PEER -eq 0 ]; then
+      CORE_PEER_ADDRESS=peer0.org2.techracers.com:7051
+    else
+      CORE_PEER_ADDRESS=peer1.org2.techracers.com:7051
+    fi
+  else
+    echo "================== ERROR !!! ORG Unknown =================="
+  fi
+
+  if [ "$VERBOSE" == "true" ]; then
+    env | grep CORE
+  fi
+}
+
+updateAnchorPeers() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel update -o orderer.techracers.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    peer channel update -o orderer.techracers.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Anchor peer update failed"
+  echo "===================== Anchor peers updated for org '$CORE_PEER_LOCALMSPID' on channel '$CHANNEL_NAME' ===================== "
+  sleep $DELAY
+  echo
+}
+
+## Sometimes Join takes time hence RETRY at least 5 times
+joinChannelWithRetry() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+
+  set -x
+  peer channel join -b $CHANNEL_NAME.block >&log.txt
+  res=$?
+  set +x
+  cat log.txt
+  if [ $res -ne 0 -a $COUNTER -lt $MAX_RETRY ]; then
+    COUNTER=$(expr $COUNTER + 1)
+    echo "peer${PEER}.org${ORG} failed to join the channel, Retry after $DELAY seconds"
+    sleep $DELAY
+    joinChannelWithRetry $PEER $ORG
+  else
+    COUNTER=1
+  fi
+  verifyResult $res "After $MAX_RETRY attempts, peer${PEER}.org${ORG} has failed to join channel '$CHANNEL_NAME' "
+}
+
+installChaincode() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+  VERSION=${3:-1.0}
+  set -x
+  peer chaincode install -n mycc -v ${VERSION} -l ${LANGUAGE} -p ${CC_SRC_PATH} >&log.txt
+  res=$?
+  set +x
+  cat log.txt
+  verifyResult $res "Chaincode installation on peer${PEER}.org${ORG} has failed"
+  echo "===================== Chaincode is installed on peer${PEER}.org${ORG} ===================== "
+  echo
+}
+
+instantiateChaincode() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+  VERSION=${3:-1.0}
+
+  # while 'peer chaincode' command can get the orderer endpoint from the peer
+  # (if join was successful), let's supply it directly as we know it using
+  # the "-o" option
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer chaincode instantiate -o orderer.techracers.com:7050 -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init","key","100"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')" >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    # peer chaincode instantiate -o orderer.techracers.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init", "{\"locations\":{\"longitude\":\"10.10\",\"latitude\":\"10.10\",\"altitude\":\"10.10\"},\"technology\":\"LTE\",\"technologyOptions\":\"Rel 13\",\"qualityOfService\":{\"totalAvailableBandwidth\":\"10\",\"peakUplinkSpeed\":\"20\"},\"accessControlType\":\"PLMN\",\"userIdentity\":\"user code of buyers users\",\"routingType\":\"LB\",\"metricOfValue\":{\"IMSI\":\"91\"},\"timePeriod\":{\"from\":\"some epoch time\",\"to\":\"some other epoch time > from\"},\"price\":{\"perGB\":\"10\"},\"pricingType\":\"one time\",\"recurringFrequency\":\"daily\",\"onetimeDuratrion\":\"1 year\",\"paymentTerms\":\"up-front\",\"prepaidAmount\":\"100\",\"topup amount\":\"100\",\"renewals\":\"5 days\",\"contractSeller\":\"seller ID\",\"contractBuyer\":\"buyer ID\",\"contractStatus\":\"inactive\",\"performanceReporting\":[\"number of unique users\",\"square feet utilized\"],\"reportingTolerance\":\"10\",\"reportingFrequency\":\"monthly\"}"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')" >&log.txt
+    # 
+    # 
+    peer chaincode instantiate -o orderer.techracers.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init","{\"name\": \"Simple Token\", \"symbol\": \"SMT\"}"]}' -P "OR ('Org1MSP.peer','Org2MSP.peer')" >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Chaincode instantiation on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' failed"
+  echo "===================== Chaincode is instantiated on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' ===================== "
+  echo
+}
+
+# fetchChannelConfig <channel_id> <output_json>
+# Writes the current channel config for a given channel to a JSON file
+fetchChannelConfig() {
+  CHANNEL=$1
+  OUTPUT=$2
+
+  setOrdererGlobals
+
+  echo "Fetching the most recent configuration block for the channel"
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel fetch config config_block.pb -o orderer.techracers.com:7050 -c $CHANNEL --cafile $ORDERER_CA
+    set +x
+  else
+    set -x
+    peer channel fetch config config_block.pb -o orderer.techracers.com:7050 -c $CHANNEL --tls --cafile $ORDERER_CA
+    set +x
+  fi
+
+  echo "Decoding config block to JSON and isolating config to ${OUTPUT}"
+  set -x
+  configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config >"${OUTPUT}"
+  set +x
+}
+
+# signConfigtxAsPeerOrg <org> <configtx.pb>
+# Set the peerOrg admin of an org and signing the config update
+signConfigtxAsPeerOrg() {
+  PEERORG=$1
+  TX=$2
+  setGlobals 0 $PEERORG
+  set -x
+  peer channel signconfigtx -f "${TX}"
+  set +x
+}
+
+# createConfigUpdate <channel_id> <original_config.json> <modified_config.json> <output.pb>
+# Takes an original and modified config, and produces the config update tx
+# which transitions between the two
+createConfigUpdate() {
+  CHANNEL=$1
+  ORIGINAL=$2
+  MODIFIED=$3
+  OUTPUT=$4
+
+  set -x
+  configtxlator proto_encode --input "${ORIGINAL}" --type common.Config >original_config.pb
+  configtxlator proto_encode --input "${MODIFIED}" --type common.Config >modified_config.pb
+  configtxlator compute_update --channel_id "${CHANNEL}" --original original_config.pb --updated modified_config.pb >config_update.pb
+  configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate >config_update.json
+  echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . >config_update_in_envelope.json
+  configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope >"${OUTPUT}"
+  set +x
+}
